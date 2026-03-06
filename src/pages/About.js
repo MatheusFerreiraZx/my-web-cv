e2875201f5df38f7c0bcf6b4747cf09409fa6349
Mac <mac@MacBook-Pro-de-Mac.local>
2026-03-06 08:55:09 -0300

    feat: change some details

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,11 +7,13 @@ const About = () => {
       <section className="mb-12">
         <h2 className="text-2xl font-bold mb-4">About Me</h2>
         <p className="text-text-secondary mb-4">
-         Software Engineer with 5 years of experience and a strong experience in building large-scale, high-impact apps — especially in the financial industry.
-
-      Having worked for leading banks and fintechs, I bring a solid understanding of scalability, performance, and user trust. 
-
-      Skilled in Swift and modern software architectures, I’m now focused on elevating mobile experiences by combining code with design, motion, and branding.
+          Software Engineer with 5 years of experience and a strong experience
+          in building large-scale, high-impact apps — especially in the
+          financial industry. Having worked for leading banks and fintechs, I
+          bring a solid understanding of scalability, performance, and user
+          trust. Skilled in Swift and modern software architectures, I’m now
+          focused on elevating mobile experiences by combining code with design,
+          motion, and branding.
         </p>
       </section>
 
@@ -22,48 +24,6 @@ const About = () => {
 
       <section className="mb-12">
         <h2 className="text-2xl font-bold mb-4">Work Experience</h2>
-
-        <div className="mb-6">
-          <h3 className="text-xl font-semibold mb-2">NTT DATA</h3>
-          <h4 className="text-lg">Senior iOS Developer</h4>
-          <p className="text-sm text-text-secondary">2025 - 2026</p>
-          {/* <p className="mt-2">
-            <ul class="list-disc pl-5">
-              <li>
-                High-Impact Applications: Contributed to the development and
-                enhancement of applications used by over 100 million users, with
-                a focus on delivering seamless, user-friendly experiences.
-              </li>
-              <li> 
-                iOS Expertise: Leveraged Swift, UIKit, and SwiftUI to improve
-                and maintain iOS applications, ensuring high performance and
-                user satisfaction.
-              </li>
-              <li>
-                PWA Development: Designed and built Progressive Web Apps (PWAs)
-                for Brazilian telecom operators, delivering a mobile-like
-                experience that optimized user interaction.
-              </li>
-              <li>
-                Feature Integration: Successfully integrated complex features,
-                such as mobile recharge within the TikTok app, through
-                collaboration with external telecom partners, enhancing
-                functionality and user engagement.
-              </li>
-              <li>
-                Mentorship & Team Collaboration: Guided new interns and
-                supported their onboarding, while actively participating in
-                Scrum and Kanban processes, contributing to task management and
-                team communication.
-              </li>
-              <li>
-                Performance & Observability: Utilized observability tools to
-                monitor and maintain application health, ensuring a consistently
-                positive user experience.
-              </li>
-            </ul>
-          </p> */}
-        </div>
 
         <div className="mb-6">
           <h3 className="text-xl font-semibold mb-2">IBM</h3>
@@ -106,7 +66,6 @@ const About = () => {
             </ul>
           </p>
         </div>
-
 
         <div className="mb-6">
           <h3 className="text-xl font-semibold mb-2">BEMOBI</h3>
